ecf9f801a3b05d69d909f8e98f347aeea60e437d
Kirill Kor <grav.kor@yandex.ru>
2023-03-23 23:49:37 +0500

    validation movieId

--- routes/movies.js
+++ routes/movies.js
@@ -23,7 +23,7 @@ router.post('/', celebrate({
 
 router.delete('/:movieId', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().hex().length(24),
+    movieId: Joi.number(),
   }),
 }), deleteMovie);
 
